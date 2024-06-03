e8c4a60cd98ae9be8d6d118e97378ffbe9e00790
fatu177 <fatu.dzaki@gmail.com>
2024-06-03 14:14:21 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 Route::prefix('admin')->group(function () {
     Route::get('/', '\App\Http\Controllers\LoginController@index');
+    Route::post('/', '\App\Http\Controllers\LoginController@login')->name('login.post');
+    Route::get('/logout', '\App\Http\Controllers\LoginController@logout')->name('logout');
+    Route::middleware(['auth'])->group(function () {
+        Route::get('/dashboard', '\App\Http\Controllers\DashboardController@index')->name('dashboard');
+        Route::resource('/education', '\App\Http\Controllers\EducationController');
+    });
 });
